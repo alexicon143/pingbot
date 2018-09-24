178e6c95f4e0e657d5338a9390eefa228fc1a462
Alex Smith <alex.smith@luricidex.com>
2018-09-23 21:17:52 -0700

    More functionality

--- pingbot.sh
+++ pingbot.sh
@@ -4,17 +4,26 @@
 do_ping () {
 ping $1 -c 1 -I eth0
 }
+do_curl () {
+curl $1 > /dev/null
+}
 
 i=0
 while true 
   	do
-	i++
+	((i++))
   	do_ping "8.8.4.4"
-#	if i == 5
-#		then
-#		do_ping "meraki.com"
-#		i=0
-#	fi
+	if ((i == 5))
+		then
+		do_ping "meraki.com"
+		do_curl "meraki.com"
+	fi
+	if ((i == 5))
+		then
+		do_ping "microsoft.com"
+		do_curl "microsoft.com"
+		i=0
+	fi
 	sleep 1
 	done
 end
